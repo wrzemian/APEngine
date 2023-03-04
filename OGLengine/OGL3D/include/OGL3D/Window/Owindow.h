60d636effb6a5b5363f5d0ada1b9a0993908e85b
Michał Wielgus <236706@edu.p.lodz.pl>
2023-03-04 18:37:30 +0100

    creating and handling window

---
+++ OGLengine/OGL3D/include/OGL3D/Window/Owindow.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class OWindow
+{
+public:
+	OWindow();
+	~OWindow();
+
+	void onDestroy();
+	bool isClosed();
+private:
+	void* m_handle = nullptr;
+};
---
+++ OGLengine/OGL3D/source/OGL3D/Game/OGame.cpp
@@ -0,0 +1,43 @@
+#include <OGL3D/Game/OGame.h>
+#include <OGL3D/Window/OWindow.h>
+#include <Windows.h>
+
+OGame::OGame()
+{
+	m_display = std::make_unique<OWindow>();
+}
+
+OGame::~OGame()
+{
+}
+
+void OGame::run()
+{
+
+	while (m_isRunning && m_display->isClosed())
+	{
+		MSG msg = {};
+		if (PeekMessage(&msg, HWND(), NULL, NULL, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				m_isRunning = false;
+				continue;
+			}
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			Sleep(1);
+		}
+	}
+}
+
+void OGame::quit()
+{
+	m_isRunning = false;
+}
