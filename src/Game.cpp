de90397ace7dcdc33b8710d85ac92506ca5753d3
236694 Wielgus Michal <236694@edu.p.lodz.pl>
2023-04-21 20:44:24 +0200

    deleted commented lines from Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -28,26 +28,15 @@
 #include "al.h"
 #include "alc.h"
 
-//void processInput(GLFWwindow* window);
-
-
 namespace Game {
     void processInput(GLFWwindow* window);
 
     void ImGui();
 
-    const int SCR_WIDTH = 1000;
-    const int SCR_HEIGHT = 800;
-
-    GLFWwindow* window;
     HUD hud;
     Shader shader;
 
-//    Object3D floor;
-//    Object3D box;
     MovingObject movingObject;
-
-//    Object3D walls;
     Object3D wagon;
 
     Camera camera(glm::vec3(0.f, 5.0f, 30.0f));
@@ -60,48 +49,25 @@ namespace Game {
 
     InputSystem inputSystem;
 
-//    Hitbox hitbox1;
-//    Hitbox floor;
-    //DebugShape debugShape;
-float x = 0;
-float y = 0;
 
     void Start() {
-        std::cout << Engine::Init();
-
+        std::cout << Engine::Init() <<"\n";
 
         ALCdevice *device;
-
         device = alcOpenDevice(NULL);
 
-        window = Engine::getWindow();
         inputSystem.InputInit();
         inputSystem.monitorKey(GLFW_KEY_A);
         inputSystem.monitorKey(GLFW_KEY_SPACE);
 
-//        hud.initImage("res/textures/tlo.png");
         hud.initAnimation();
         hud.initText("res/fonts/Arialn.ttf");
-//        floor.loadModel("../../res/models/first box/skrzynia.obj");
-//        floor._transform._scale = glm::vec3(50, 1, 50);
-//        box.loadModel("../../res/models/second box/skrzynia2.obj");
-
         movingObject.loadModel("../../res/models/first_character/first character.obj");
-//        movingObject._transform._scale = glm::vec3(0.2f);
-//        walls.loadModel("../../res/models/walls/walls.obj");
+
         wagon.loadModel("../../res/models/1level/1level.obj");
         for(auto m: wagon._model.meshes) {
             spdlog::info("loaded mesh with {} vertices", m.vertices.size());
         }
-//        hitbox1.Create(&movingObject, glm::vec3(1,3,2));
-//        hitbox1._min = glm::vec3(-1, -1, -1);
-//        hitbox1._max = glm::vec3(1, 1, 1);
-
-//        Object3D f;
-//        floor.Create(&f);
-//        floor._min = glm::vec3(-20, -5, -20);
-//        floor._max = glm::vec3(20, 0, 20);
-//        floor._color = glm::vec3(1,1,0);
 
         // build and compile our shader program
         // ------------------------------------
@@ -110,7 +76,6 @@ float y = 0;
         shader.use();
 
         movingObject.setShader(&shader);
-//        walls.setShader(&shader);
         wagon.setShader(&shader);
 
         shader.setMat4("projectionView", camera.viewProjection);
@@ -125,58 +90,25 @@ float y = 0;
         pointLight = parser.CreateFromJSONPoint("pointLight");
 
 
-        parser.SaveJSON(dirLight.ParseToJSON(), "dirLight");
-//        parser.SaveJSON(spotLight.ParseToJSON(), "spotLight");
-//        parser.SaveJSON(pointLight.ParseToJSON(), "pointLight");
-
-
-        while (!glfwWindowShouldClose(window)) {
+        while (!glfwWindowShouldClose(Engine::getWindow())) {
              Update();
         }
     }
 
     void Update() {
-
         Engine::LoopStart();
         ImGui();
-        x -= 0.3;
-
 
         inputSystem.update();
 
-        if (inputSystem.GetKey(GLFW_KEY_A))
-            std::cout << "escape1" << std::endl; // we have library for logging...
-        if (inputSystem.GetKeyDown(GLFW_KEY_A))
-            std::cout << "aaaa" << std::endl;
-/*
-        float xAxisGamepad1 = inputSystem.getJoystickAxis(0, GLFW_GAMEPAD_AXIS_LEFT_X);
-        float yAxisGamepad1 = inputSystem.getJoystickAxis(0, GLFW_GAMEPAD_AXIS_LEFT_Y);
-        std::cout << "Gamepad 1 X-axis: " << xAxisGamepad1 << ", Y-axis: " << yAxisGamepad1 << std::endl;
 
-        float xAxisGamepad2 = inputSystem.getJoystickAxis(1, GLFW_GAMEPAD_AXIS_LEFT_X);
-        float yAxisGamepad2 = inputSystem.getJoystickAxis(1, GLFW_GAMEPAD_AXIS_LEFT_Y);
-        std::cout << "Gamepad 2 X-axis: " << xAxisGamepad2 << ", Y-axis: " << yAxisGamepad2 << std::endl;
-*/
-        if (inputSystem.GetGamepadButtonDown(0, GLFW_GAMEPAD_BUTTON_A)) {
-            std::cout << "Gamepad 1 A button just got pressed." << std::endl;
-        }
-
-        if (inputSystem.GetGamepadButtonDown(1, GLFW_GAMEPAD_BUTTON_A)) {
-            std::cout << "Gamepad 2 A button just got pressed." << std::endl;
-        }
 
         movingObject.Move();
-        //player1.Draw(shader);
-        //floor.Draw(shader);
+
         movingObject.Draw();
         wagon.Draw();
 
-
-//        walls.Draw();
         //box.Draw(shader);
-
-
-
 
         ////REQUIRED FOR LIGHT
         //should be property of object
@@ -191,7 +123,7 @@ float y = 0;
         pointLight.SendToShader(shader, "pointLight");
 
 
-        processInput(window);
+        processInput(Engine::getWindow());
 
 
         //camera
@@ -199,15 +131,8 @@ float y = 0;
         glm::mat4 projection = glm::mat4(1.0f);
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
-
         glm:: mat4 view = camera.getView(movingObject);
 
-//        hitbox1.Draw(projection*view);
-//        floor.Draw(projection*view);
-
-        //hitbox1.TestForIntersection(floor);
-
-        //debugShape.DrawCube(glm::vec3(0), glm::vec3(1, 1, 1), glm::vec4(0), projection* view);
         shader.use();
         shader.setMat4("projectionView", projection * view);
         camera.followObject(movingObject);
@@ -222,21 +147,6 @@ float y = 0;
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
         {
-            //player1.ImGui();
-//            camera.ImGui();
-//            pointLight.ImGui();
-//            spotLight.ImGui();
-//            dirLight.ImGui();
-//
-//            movingObject.ImGui();
-//
-////            floor.ImGui("Floor");
-////            box.ImGui("Box");
-//
-//            hitbox1.ImGui("Player hitbox");
-//            floor.ImGui("Floor hitbox");
-//            hud.ImGui();
-//            hud.imguiText();
             Engine::renderImgui();
             Engine::ImGui();
 
