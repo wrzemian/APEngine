150516d2db038d14cc4ff20f102114353e397b8a
Tomasz Lesiak <tomek.l.lesiak@gmail.com>
2023-04-26 18:48:58 +0200

    Constant element

---
+++ include/Constant.h
@@ -0,0 +1,26 @@
+#ifndef APENGINE_CONSTANT_H
+#define APENGINE_CONSTANT_H
+
+#include <iostream>
+#include "glad/glad.h"
+#include "Shader.h"
+
+
+
+class Constant {
+private:
+    Shader constShader;
+    GLuint constVAO, constVBO, constTexture;
+    float constTime;
+
+public:
+    Constant(const Shader _constShader);
+
+
+    void initConstant();
+    void renderConstant();
+
+    Constant();
+};
+
+#endif //APENGINE_CONSTANT_H
