e67e6ad62fab06a519c153ee875bc87d3115aabd
Tomasz Lesiak <tomek.l.lesiak@gmail.com>
2023-04-26 18:32:08 +0200

    Animation

---
+++ include/Animation.h
@@ -0,0 +1,29 @@
+#ifndef APENGINE_ANIMATION_H
+#define APENGINE_ANIMATION_H
+
+#include <iostream>
+#include "glad/glad.h"
+#include "Shader.h"
+
+
+
+class Animation{
+public:
+    Animation(Shader _animationShader);
+
+    Animation();
+
+    void initAnimation();
+
+    void renderAnimation(float time);
+private:
+    Shader animationShader;
+    GLuint animationShaderProgram;
+    GLuint animationVAO, animationVBO, animationTexture;
+    float animationTime;
+
+
+};
+
+
+#endif //APENGINE_ANIMATION_H
