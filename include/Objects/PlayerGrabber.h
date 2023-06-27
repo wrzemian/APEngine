94f4e85f49d71fd3467e552f34c4f9c8315607da
Karol D <karold@tutanota.com>
2023-06-28 00:20:29 +0200

    corrected offsets

--- include/Objects/PlayerGrabber.h
+++ include/Objects/PlayerGrabber.h
@@ -21,8 +21,8 @@ private:
     int walking;
     int recentlyMoved;
 
-    glm::vec3 batteryOffset = glm::vec3(-0.25f, 1.0f, 0.0f);
-    glm::vec3 boxOffset = glm::vec3(1.0f, 1.0f, 0.0f);
+    glm::vec3 batteryOffset = glm::vec3(-0.45f, 0.7f, 0.0f);
+    glm::vec3 boxOffset = glm::vec3(0.5f, 0.25f, 0.0f);
     glm::vec3 direction =  glm::vec3(0.0f, 0.0f, 0.0f);
     float padJoystickTolerance = 0.2f;
 public:
